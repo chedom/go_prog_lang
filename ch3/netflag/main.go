627cb47b847f38413c9b1a585a98b000837ae2db
Vasyl Domanchuk <domanchukits@gmail.com>
2018-11-21 08:02:08 +0200

    finish chapter 3

---
+++ ch3/netflag/main.go
@@ -0,0 +1,16 @@
+package netflag
+
+type Flags uint
+
+const (
+	FlagUp Flags = 1 << iota
+	FlagBroadcast
+	FlagLoopback
+	FlagPointToPoint
+	FlagMulticast
+)
+
+func IsUp(v Flags) bool     { return v&FlagUp == FlagUp }
+func TurnDown(v *Flags)     { *v &^= FlagUp }
+func SetBroadcast(v *Flags) { *v |= FlagBroadcast }
+func IsCast(v Flags) bool   { return v&(FlagBroadcast|FlagMulticast) != 0 }
