ce07c3eac3c3ddf0fb21a4cd1cf78103fa1b610e
Vasyl Domanchuk <domanchukits@gmail.com>
2019-01-28 07:11:37 +0200

    add exercise 5.5

---
+++ ch5/ex5.5/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func CountWordsAndImages(url string) (words, images int, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		err = fmt.Errorf("parsing HTML: %v", err)
+		return
+	}
+	words, images = countWordsAndImages(doc)
+	return
+}
+
+func countWordsAndImages(n *html.Node) (words, images int) {
+	if n.Type == html.ElementNode && n.Data == "img" {
+		images++
+	}
+	if n.Type == html.TextNode {
+		scanner := bufio.NewScanner(strings.NewReader(n.Data))
+		scanner.Split(bufio.ScanWords)
+		for scanner.Scan() {
+			words++
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		newWords, newImages := countWordsAndImages(c)
+		words += newWords
+		images += newImages
+	}
+
+	return words, images
+}
+
+func main() {
+	for _, url := range os.Args[1:] {
+		words, images, err := CountWordsAndImages(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "scanning %s %v\n", url, err)
+			continue
+		}
+		fmt.Printf("url: %s, words: %d, images: %d\n", url, words, images)
+	}
+}
