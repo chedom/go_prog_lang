57abaaef5b72b20c63dd4d21d17bd504a43fe5cc
Vasyl Domanchuk <domanchukits@gmail.com>
2019-02-12 07:37:31 +0200

    add exersice 7.7

---
+++ ch7/ex7.7/main.go
@@ -0,0 +1,65 @@
+package multisort
+
+import (
+	"time"
+)
+
+type Track struct {
+	Title string
+	Artist string
+	Album string
+	Year int
+	Length time.Duration
+}
+
+// define sort function
+type LessFunc func(i, j *Track) bool
+func sortByTitle(i, j *Track) bool { return i.Title < j.Title }
+func sortByArtist(i, j *Track) bool { return i.Artist < j.Artist }
+func sortByAlbum(i, j *Track) bool { return i.Album < j.Album }
+func sortByYear(i, j *Track) bool { return i.Year < j.Year }
+func sortByLength(i, j *Track) bool { return i.Length < j.Length }
+
+var sortDict = map[string]LessFunc{
+	"title": sortByTitle,
+	"artist": sortByArtist,
+	"album": sortByAlbum,
+	"year": sortByYear,
+	"length": sortByLength,
+}
+
+type MultiSort struct {
+	t []*Track
+	compares []LessFunc
+}
+
+func (m *MultiSort) Len() int { return len(m.t) }
+func (m *MultiSort) Swap(i, j int) { m.t[i], m.t[j] = m.t[j], m.t[i] }
+func (m *MultiSort) Less(i,j int) bool {
+	p, q := m.t[i], m.t[j]
+	var k int
+	for k = 0; k < len(m.compares) - 1; k++ {
+		less := m.compares[k]
+		switch  {
+		case less(p, q):
+			return true
+		case less(q, p):
+			return false
+		}
+		// p == q; try the next comparison.
+	}
+
+	return m.compares[k](p, q)
+}
+
+func NewMultiSort(tracks []*Track, ordering []string) *MultiSort {
+	compares := make([]LessFunc, 0)
+	for _, v := range ordering {
+		if f, ok := sortDict[v]; ok {
+			compares = append(compares, f)
+		}
+	}
+
+	return &MultiSort{t:tracks, compares: compares}
+}
+
---
+++ ch7/sorting/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"text/tabwriter"
+	"time"
+)
+
+type Track struct {
+	Title string
+	Artist string
+	Album string
+	Year int
+	Length time.Duration
+}
+
+func length(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(s)
+	}
+	return d
+}
+
+var tracks = []*Track{
+	{"Go", "Delialah", "Froom the Roots Up", 2012, length("3m38s")},
+	{"Go", "Moby", "Moby", 1992, length("3m37s")},
+	{"Go Ahead", "Alicia Keys", "As i Am", 2007, length("4m36s")},
+	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
+}
+
+func printTracks(tracks []*Track) {
+	const format = "%v\t%v\t%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+	fmt.Fprintf(tw, format, "-----", "-----", "-----", "-----", "-----")
+	for _, t := range tracks {
+		fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
+	}
+	tw.Flush()
+}
+
+// sort by artist
+type byArtist []*Track
+
+func (x byArtist) Len() int { return len(x) }
+func (x byArtist) Less(i, j int) bool { return x[i].Artist < x[j].Artist}
+func (x byArtist) Swap(i, j int) { x[i], x[j] = x[j], x[i] }
+
+// sort by year
+type byYear []*Track
+
+func (x byYear) Len() int { return len(x)}
+func (x byYear) Less(i, j int) bool { return x[i].Year < x[j].Year }
+func (x byYear) Swap(i, j int) { x[i], x[j] = x[j], x[i] }
+
+// custom sort
+type customSort struct {
+	t []*Track
+	less func(i, j *Track) bool
+}
+
+func (c customSort) Len() int { return len(c.t) }
+func (c customSort) Less(i, j int) bool { return c.less(c.t[i], c.t[j])}
+func (c customSort) Swap(i, j int) { c.t[i], c.t[j] = c.t[j], c.t[i] }
+
+
+func main() {
+	printTracks(tracks)
+	fmt.Println()
+
+	sort.Sort(byArtist(tracks))
+	printTracks(tracks)
+	fmt.Println()
+
+	sort.Sort(sort.Reverse(byArtist(tracks)))
+	printTracks(tracks)
+	fmt.Println()
+
+	sort.Sort(byYear(tracks))
+	printTracks(tracks)
+	fmt.Println()
+
+	sort.Sort(customSort{tracks, func (i, j *Track) bool {
+		if i.Title != j.Title {
+			return i.Title < j.Title
+		}
+		if i.Year != j.Year {
+			return i.Year < j.Year
+		}
+		if i.Length < j.Length {
+			return i.Length < j.Length
+		}
+		return false
+	}})
+	printTracks(tracks)
+	fmt.Println()
+}
