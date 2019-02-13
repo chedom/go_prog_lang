0781a13ce02984fd7b5a03e0e043203116456eb5
Vasyl Domanchuk <domanchukits@gmail.com>
2019-02-13 08:01:43 +0200

    add exercise 7.10

---
+++ ch7/ex7.10/main.go
@@ -0,0 +1,19 @@
+package palindrome
+
+import "sort"
+
+func isEqual(s sort.Interface, i, j int) bool {
+	return !s.Less(i, j) && !s.Less(j, i)
+}
+
+func isPalindrome(s sort.Interface) bool {
+	lastIdx := s.Len() - 1
+	for i:=0; i < s.Len() / 2; i++ {
+		if !isEqual(s, i, lastIdx - i) {
+			return false
+		}
+
+	}
+
+	return true
+}
