9dd265d42c7e21fcd4a4922450272960d10b7437
Vasyl Domanchuk <domanchukits@gmail.com>
2019-05-01 18:42:56 +0300

    add exercise 8.5

---
+++ ch8/ex8.4/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+var port = flag.Int("port", 8080, " port for tcp server")
+
+func main() {
+	flag.Parse()
+	addr := fmt.Sprintf("localhost:%d", *port)
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Accept error: %v\n", err)
+		}
+		go handleConn(conn)
+	}
+}
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+
+	var wg sync.WaitGroup
+
+	for input.Scan() {
+		wg.Add(1)
+
+		go func(s string) {
+			defer wg.Done()
+
+			echo(c, s, 1 * time.Second)
+		}(input.Text())
+	}
+
+	wg.Wait()
+	if conn, ok := c.(*net.TCPConn); ok {
+		conn.CloseWrite()
+		return
+	}
+	c.Close()
+}
---
+++ ch8/thumbnail/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"log"
+	"os"
+	"sync"
+
+	"github.com/adonovan/gopl.io/ch8/thumbnail"
+)
+
+func makeThumbnails3(filenames []string) {
+	ch := make(chan struct{})
+
+	for _, f := range filenames {
+		go func(f string) {
+			if _, err := thumbnail.ImageFile(f); err != nil {
+				log.Println(err)
+			}
+			ch <- struct{}{}
+		}(f)
+	}
+
+	for range filenames {
+		<- ch
+	}
+}
+
+func makeThumbnails4(filenames []string) error {
+	errors := make(chan error)
+
+	for _, f := range filenames {
+		go func(f string) {
+			_, err := thumbnail.ImageFile(f)
+			errors <- err
+		}(f)
+	}
+
+	for range filenames {
+		if err := <- errors; err != nil {
+			return err // NOTE: incorrect: goroutine leak!
+		}
+	}
+
+	return nil
+}
+
+func makeThumbnails5(filenames []string) (thumbnails []string, err error) {
+	type item struct {
+		thumbfile string
+		err error
+	}
+
+	ch := make(chan item, len(filenames))
+	for _, f := range filenames {
+		go func(f string) {
+			var it item
+			it.thumbfile, it.err = thumbnail.ImageFile(f)
+			ch <- it
+		}(f)
+	}
+
+	for range filenames {
+		it := <- ch
+		if it.err != nil {
+			return nil, it.err
+		}
+		thumbnails = append(thumbnails, it.thumbfile)
+	}
+
+	return thumbnails, nil
+}
+
+func makeThumbnails6(filenames <- chan string) int64 {
+	sizes := make(chan int64)
+	var wg sync.WaitGroup
+	for f:= range filenames {
+		wg.Add(1)
+		//worker
+		go func(f string) {
+			defer wg.Done()
+			thumb, err := thumbnail.ImageFile(f)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			info, _ := os.Stat(thumb)
+			sizes <- info.Size()
+		}(f)
+	}
+
+	// closer
+	go func() {
+		wg.Wait()
+		close(sizes)
+	}()
+
+	var total int64
+	for size := range sizes {
+		total += size
+	}
+
+	return total
+}
