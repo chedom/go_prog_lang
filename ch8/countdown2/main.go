4654c00808303cec9064754bea54b58b27cef8db
Vasyl Domanchuk <domanchukits@gmail.com>
2019-05-03 22:26:39 +0300

    add exercise 8.8

---
+++ ch8/countdown2/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	fmt.Println("Commencing countdown.")
+	tick := time.Tick(1 * time.Second)
+	abort := make(chan struct{})
+
+	go func() {
+		os.Stdin.Read(make([]byte, 1)) // read a single byte
+		abort <- struct{}{}
+	}()
+
+	for countdown := 10; countdown > 0; countdown-- {
+		fmt.Println(countdown)
+
+		select {
+		case <- tick:
+			continue
+		case <- abort:
+			fmt.Println("Launch aborted!")
+			return
+		}
+	}
+}
---
+++ ch8/ex8.8/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+var port = flag.Int("port", 8080, " port for tcp server")
+
+func main() {
+	flag.Parse()
+	addr := fmt.Sprintf("localhost:%d", *port)
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Accept error: %v\n", err)
+		}
+		go handleConn(conn)
+	}
+}
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+
+	shouts := make(chan string)
+
+	go func() {
+		for input.Scan() {
+			shouts <- input.Text()
+		}
+	}()
+
+	for {
+		select {
+		case m := <- shouts:
+			go echo(c, m, 1 * time.Second)
+		case <- time.After(10 * time.Second):
+			c.Close()
+			return
+		}
+	}
+
+	c.Close()
+}
