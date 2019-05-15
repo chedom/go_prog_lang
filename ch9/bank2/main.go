ebb9b907ec2e6a5c0624ac326ea950c08c33fa86
chedom <domanchukits@gmail.com>
2019-05-15 18:56:34 +0300

    add some examples

---
+++ ch9/bank2/main.go
@@ -0,0 +1,19 @@
+package bank2
+
+var (
+	sema = make(chan struct{}, 1)
+	balance int
+)
+
+func Deposit(amount int) {
+	sema <- struct{}{} // acquire token
+	balance += amount
+	<- sema // release token
+}
+
+func Balance() int {
+	sema <- struct{}{}
+	b := balance
+	<- sema
+	return b
+}
