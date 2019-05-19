d7c5aaa5a2d40176aac7a6e15b9889ecf679393e
Vasyl Domanchuk <domanchukits@gmail.com>
2019-05-19 22:23:19 +0300

    dirty

---
+++ ch9/memo1/memo_test.go
@@ -0,0 +1,16 @@
+package memo1
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func hhtGetBody(url string) (interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
