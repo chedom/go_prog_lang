ea58d9174c8e79aea3a1666ce6b6143d0a145ebe
chedom <domanchukits@gmail.com>
2019-05-22 10:29:49 +0300

    add exercise 9.5

--- ch9/ex9.5/ping.go
+++ ch9/ex9.5/ping.go
@@ -3,8 +3,6 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
 	"time"
 )
 
@@ -12,21 +10,37 @@ func main() {
 	q := make(chan int)
 	var i int64
 	start := time.Now()
+	done := make(chan struct{})
+
+	go func() {
+		time.Sleep(5 * time.Second)
+		close(done)
+	}()
+
 	go func() {
 		q <- 1
 		for {
-			i++
-			q <- <-q
+			select {
+			case <- done:
+				return
+			case n := <- q:
+				i++
+				q <- n
+			}
 		}
 	}()
 	go func() {
 		for {
-			q <- <-q
+			select {
+			case <- done:
+				return
+			case n := <- q:
+				q <- n
+			}
 		}
 	}()
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	<-c
+	<- done
 	fmt.Println(float64(i)/float64(time.Since(start))*1e9, "round trips per second")
+
 }
