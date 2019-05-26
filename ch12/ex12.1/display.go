cadeaf94c04fed99b80ecdeb68b3745bf00024e3
Vasyl Domanchuk <domanchukits@gmail.com>
2019-05-26 14:41:27 +0300

    add exercise 12.2

---
+++ ch12/ex12.1/display.go
@@ -0,0 +1,83 @@
+package display
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/chedom/go_prog_lang/ch12/format"
+	"reflect"
+)
+
+func Display(name string, x interface{}) {
+	fmt.Printf("Display %s (%T):\n", name, x)
+	display(name, reflect.ValueOf(x))
+}
+
+func formatMapKey(v reflect.Value) string {
+	switch v.Kind() {
+
+	case reflect.Array:
+		buf := &bytes.Buffer{}
+		buf.WriteByte('{')
+		for i := 0; i < v.Len(); i++ {
+			if i != 0 {
+				buf.WriteByte(',')
+			}
+			buf.WriteString(format.FormatAtom(v.Index(i)))
+		}
+		buf.WriteByte('}')
+		return buf.String()
+
+	case reflect.Struct:
+		buf := &bytes.Buffer{}
+		buf.WriteByte('{')
+
+		for i:= 0; i < v.NumField(); i++ {
+			if i != 0 {
+				buf.WriteByte(',')
+			}
+			fmt.Fprintf(buf, "%s:%s", v.Type().Field(i).Name,
+				format.FormatAtom(v.Field(i)))
+		}
+
+		buf.WriteByte('}')
+		return buf.String()
+
+	default:
+		return format.FormatAtom(v)
+	}
+}
+
+func display(path string, v reflect.Value) {
+	switch v.Kind() {
+	case reflect.Invalid:
+		fmt.Printf("%s = invalid\n", path)
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < v.Len(); i++ {
+			display(fmt.Sprintf("%s[%d]", path, i), v.Index(i))
+		}
+	case reflect.Struct:
+		for i := 0; i < v.NumField(); i++ {
+			fieldPath := fmt.Sprintf("%s.%s", path, v.Type().Field(i).Name)
+			display(fieldPath, v.Field(i))
+		}
+	case reflect.Map:
+		for _, key := range v.MapKeys() {
+			display(fmt.Sprintf("%s[%s]", path, formatMapKey(key)), v.MapIndex(key))
+		}
+	case reflect.Ptr:
+		if v.IsNil() {
+			fmt.Printf("%s = nil\n", path)
+		} else {
+			display(fmt.Sprintf("(*%s)", path), v.Elem())
+		}
+	case reflect.Interface:
+		if v.IsNil() {
+			fmt.Printf("%s = nil\n", path)
+		} else {
+			fmt.Printf("%s.type = %s\n", path, v.Elem().Type())
+			display(path+ ".value", v.Elem())
+		}
+	default: // basic types, channels, funcs
+		fmt.Printf("%s = %s\n", path, format.FormatAtom(v))
+	}
+}
